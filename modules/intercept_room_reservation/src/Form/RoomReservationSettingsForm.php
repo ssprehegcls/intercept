6793dfd4bdaecda02d45a368476fd2aa7f87c466
Alan <alan@atendesigngroup.com>
2018-04-11 19:18:43 +0200

    Issue #2960196: Simplify urls, move content management and add config form for emails

--- modules/intercept_room_reservation/src/Form/RoomReservationSettingsForm.php
+++ modules/intercept_room_reservation/src/Form/RoomReservationSettingsForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\intercept_room_reservation\Form;
 
+use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
@@ -10,7 +11,14 @@ use Drupal\Core\Form\FormStateInterface;
  *
  * @ingroup intercept_room_reservation
  */
-class RoomReservationSettingsForm extends FormBase {
+class RoomReservationSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['intercept_core.room_reservations'];
+  }
 
   /**
    * Returns a unique string identifying the form.
@@ -19,19 +27,7 @@ class RoomReservationSettingsForm extends FormBase {
    *   The unique string identifying the form.
    */
   public function getFormId() {
-    return 'roomreservation_settings';
-  }
-
-  /**
-   * Form submission handler.
-   *
-   * @param array $form
-   *   An associative array containing the structure of the form.
-   * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   The current state of the form.
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    // Empty implementation of the abstract submit class.
+    return 'room_reservation_settings';
   }
 
   /**
@@ -46,8 +42,73 @@ class RoomReservationSettingsForm extends FormBase {
    *   Form definition array.
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['roomreservation_settings']['#markup'] = 'Settings form for Room reservation entities. Manage field settings here.';
+    $form = parent::buildForm($form, $form_state);
+    $config = $this->config('intercept_core.room_reservations');
+    $form['email'] = [
+      '#type' => 'vertical_tabs',
+      '#title' => $this->t('Emails'),
+      '#tree' => TRUE,
+    ];
+
+    $emails = [
+      'reservation_accepted' => $this->t('Reservation accepted (by staff)'),
+      'reservation_rejected' => $this->t('Reservation rejected (by staff)'),
+      'reservation_canceled' => $this->t('Reservation canceled (by staff)'),
+    ];
+
+    foreach ($emails as $key => $title) {
+      $form[$key] = [
+        '#type' => 'details',
+        '#title' => $title,
+        '#group' => 'email',
+        '#tree' => TRUE,
+      ];
+      $form[$key]['subject'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Subject'),
+        '#default_value' => $this->getSubject($key),
+        '#maxlength' => 180,
+      ];
+      $form[$key]['body'] = [
+        '#type' => 'textarea',
+        '#title' => $this->t('Body'),
+        '#default_value' => $this->getBody($key),
+        '#rows' => 15,
+      ];
+    }
     return $form;
+  }
+
+  private function getEmailConfig($key) {
+    return $this->config('intercept_core.room_reservations')->get("email.$key");
+  }
+
+  private function getSubject($key) {
+    return !empty($this->getEmailConfig($key)) ? $this->getEmailConfig($key)['subject'] : '';
+  }
+
+  private function getBody($key) {
+    return !empty($this->getEmailConfig($key)) ? $this->getEmailConfig($key)['body'] : '';
+  }
+
+  /**
+   * Form submission handler.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $config = $this->config('intercept_core.room_reservations');
+    $values = $form_state->cleanValues()->getValues();
+    unset($values['email']);
+    foreach ($values as $key => $info) {
+      ksm($info);
+      $config->set("email.$key", $info);
+    }
+    $config->save();
+    parent::submitForm($form, $form_state);
   }
 
 }
