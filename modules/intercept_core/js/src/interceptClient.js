ab99a0554556b4e12972d3c6e1b9f6695be4f2e2
John Ferris <john@atendesigngroup.com>
2018-04-24 17:21:21 -0600

    Add the ability to save a room reservation.

--- modules/intercept_core/js/src/interceptClient.js
+++ modules/intercept_core/js/src/interceptClient.js
@@ -6,6 +6,7 @@ import intercept from 'intercept-client';
 import drupalSettings from 'drupalSettings';
 import thunk from 'redux-thunk';
 import * as select from './lib/select';
+import * as utils from './lib/utils';
 import history from './lib/history';
 
 const reducers = combineReducers({
@@ -18,6 +19,6 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 /* eslint-enable */
 
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
-const interceptClient = Object.assign({}, intercept, { store, select, consumer, history });
+const interceptClient = Object.assign({}, intercept, { store, select, consumer, history, utils });
 window.interceptClient = interceptClient;
 export default interceptClient;
