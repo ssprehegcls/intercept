2d08455cca02f598b5ccce9715a87b3217a7b8da
Alan <alan@atendesigngroup.com>
2018-04-30 19:24:52 +0200

    Adds cancel/approve/deny actions and routes with forms

--- modules/intercept_room_reservation/src/RoomReservationHtmlRouteProvider.php
+++ modules/intercept_room_reservation/src/RoomReservationHtmlRouteProvider.php
@@ -46,7 +46,38 @@ class RoomReservationHtmlRouteProvider extends AdminHtmlRouteProvider {
       $collection->add("$entity_type_id.settings", $settings_form_route);
     }
 
+    foreach (['cancel', 'approve', 'decline'] as $action) {
+      if ($settings_form_route = $this->getUpdateStatusFormRoutes($entity_type, $action)) {
+        $collection->add("entity.$entity_type_id.{$action}_form", $settings_form_route);
+      }
+    }
+
     return $collection;
+  }
+
+  protected function getUpdateStatusFormRoutes($entity_type, $operation) {
+    if ($entity_type->hasLinkTemplate("{$operation}-form")) {
+      $entity_type_id = $entity_type->id();
+      $operation_label = ucwords($operation);
+      $route = new Route($entity_type->getLinkTemplate("{$operation}-form"));
+      // Use the edit form handler, if available, otherwise default.
+      $route
+        ->setDefaults([
+          '_entity_form' => "{$entity_type_id}.{$operation}",
+          '_title' => "{$operation_label} {$entity_type->getLabel()}",
+        ])
+        ->setRequirement('_entity_access', "{$entity_type_id}.{$operation}")
+        ->setOption('parameters', [
+            $entity_type_id => ['type' => 'entity:' . $entity_type_id],
+        ]);
+
+      // Entity types with serial IDs can specify this in their route
+      // requirements, improving the matching process.
+      if ($this->getEntityTypeIdKeyType($entity_type) === 'integer') {
+        $route->setRequirement($entity_type_id, '\d+');
+      }
+      return $route;
+    }
   }
 
   /**
