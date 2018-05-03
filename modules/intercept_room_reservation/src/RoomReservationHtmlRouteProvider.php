c0aa0ec62ad8ac028019192f936e5cdb7fbce014
Alan <alan@atendesigngroup.com>
2018-05-03 03:33:23 +0200

    RICHAPP-583: Adds revision handlers and optimizes exist revision handlers

--- modules/intercept_room_reservation/src/RoomReservationHtmlRouteProvider.php
+++ modules/intercept_room_reservation/src/RoomReservationHtmlRouteProvider.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\intercept_room_reservation;
 
+use Drupal\Core\Entity\EntityType;
 use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Entity\Routing\AdminHtmlRouteProvider;
 use Symfony\Component\Routing\Route;
@@ -22,24 +23,8 @@ class RoomReservationHtmlRouteProvider extends AdminHtmlRouteProvider {
 
     $entity_type_id = $entity_type->id();
 
-    if ($history_route = $this->getHistoryRoute($entity_type)) {
-      $collection->add("entity.{$entity_type_id}.version_history", $history_route);
-    }
-
-    if ($revision_route = $this->getRevisionRoute($entity_type)) {
-      $collection->add("entity.{$entity_type_id}.revision", $revision_route);
-    }
-
-    if ($revert_route = $this->getRevisionRevertRoute($entity_type)) {
-      $collection->add("entity.{$entity_type_id}.revision_revert", $revert_route);
-    }
-
     if ($delete_route = $this->getRevisionDeleteRoute($entity_type)) {
-      $collection->add("entity.{$entity_type_id}.revision_delete", $delete_route);
-    }
-
-    if ($translation_route = $this->getRevisionTranslationRevertRoute($entity_type)) {
-      $collection->add("{$entity_type_id}.revision_revert_translation_confirm", $translation_route);
+      $collection->add("entity.{$entity_type_id}.revision_delete_form", $delete_route);
     }
 
     if ($settings_form_route = $this->getSettingsFormRoute($entity_type)) {
@@ -56,6 +41,7 @@ class RoomReservationHtmlRouteProvider extends AdminHtmlRouteProvider {
   }
 
   protected function getUpdateStatusFormRoutes($entity_type, $operation) {
+    /** @var $entity_type EntityTypeInterface */
     if ($entity_type->hasLinkTemplate("{$operation}-form")) {
       $entity_type_id = $entity_type->id();
       $operation_label = ucwords($operation);
@@ -81,78 +67,6 @@ class RoomReservationHtmlRouteProvider extends AdminHtmlRouteProvider {
   }
 
   /**
-   * Gets the version history route.
-   *
-   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
-   *   The entity type.
-   *
-   * @return \Symfony\Component\Routing\Route|null
-   *   The generated route, if available.
-   */
-  protected function getHistoryRoute(EntityTypeInterface $entity_type) {
-    if ($entity_type->hasLinkTemplate('version-history')) {
-      $route = new Route($entity_type->getLinkTemplate('version-history'));
-      $route
-        ->setDefaults([
-          '_title' => "{$entity_type->getLabel()} revisions",
-          '_controller' => '\Drupal\intercept_room_reservation\Controller\RoomReservationController::revisionOverview',
-        ])
-        ->setRequirement('_permission', 'access room reservation revisions')
-        ->setOption('_admin_route', TRUE);
-
-      return $route;
-    }
-  }
-
-  /**
-   * Gets the revision route.
-   *
-   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
-   *   The entity type.
-   *
-   * @return \Symfony\Component\Routing\Route|null
-   *   The generated route, if available.
-   */
-  protected function getRevisionRoute(EntityTypeInterface $entity_type) {
-    if ($entity_type->hasLinkTemplate('revision')) {
-      $route = new Route($entity_type->getLinkTemplate('revision'));
-      $route
-        ->setDefaults([
-          '_controller' => '\Drupal\intercept_room_reservation\Controller\RoomReservationController::revisionShow',
-          '_title_callback' => '\Drupal\intercept_room_reservation\Controller\RoomReservationController::revisionPageTitle',
-        ])
-        ->setRequirement('_permission', 'access room reservation revisions')
-        ->setOption('_admin_route', TRUE);
-
-      return $route;
-    }
-  }
-
-  /**
-   * Gets the revision revert route.
-   *
-   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
-   *   The entity type.
-   *
-   * @return \Symfony\Component\Routing\Route|null
-   *   The generated route, if available.
-   */
-  protected function getRevisionRevertRoute(EntityTypeInterface $entity_type) {
-    if ($entity_type->hasLinkTemplate('revision_revert')) {
-      $route = new Route($entity_type->getLinkTemplate('revision_revert'));
-      $route
-        ->setDefaults([
-          '_form' => '\Drupal\intercept_room_reservation\Form\RoomReservationRevisionRevertForm',
-          '_title' => 'Revert to earlier revision',
-        ])
-        ->setRequirement('_permission', 'revert all room reservation revisions')
-        ->setOption('_admin_route', TRUE);
-
-      return $route;
-    }
-  }
-
-  /**
    * Gets the revision delete route.
    *
    * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
@@ -162,8 +76,8 @@ class RoomReservationHtmlRouteProvider extends AdminHtmlRouteProvider {
    *   The generated route, if available.
    */
   protected function getRevisionDeleteRoute(EntityTypeInterface $entity_type) {
-    if ($entity_type->hasLinkTemplate('revision_delete')) {
-      $route = new Route($entity_type->getLinkTemplate('revision_delete'));
+    if ($entity_type->hasLinkTemplate('revision-delete-form')) {
+      $route = new Route($entity_type->getLinkTemplate('revision-delete-form'));
       $route
         ->setDefaults([
           '_form' => '\Drupal\intercept_room_reservation\Form\RoomReservationRevisionDeleteForm',
@@ -175,31 +89,6 @@ class RoomReservationHtmlRouteProvider extends AdminHtmlRouteProvider {
       return $route;
     }
   }
-
-  /**
-   * Gets the revision translation revert route.
-   *
-   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
-   *   The entity type.
-   *
-   * @return \Symfony\Component\Routing\Route|null
-   *   The generated route, if available.
-   */
-  protected function getRevisionTranslationRevertRoute(EntityTypeInterface $entity_type) {
-    if ($entity_type->hasLinkTemplate('translation_revert')) {
-      $route = new Route($entity_type->getLinkTemplate('translation_revert'));
-      $route
-        ->setDefaults([
-          '_form' => '\Drupal\intercept_room_reservation\Form\RoomReservationRevisionRevertTranslationForm',
-          '_title' => 'Revert to earlier revision of a translation',
-        ])
-        ->setRequirement('_permission', 'revert all room reservation revisions')
-        ->setOption('_admin_route', TRUE);
-
-      return $route;
-    }
-  }
-
   /**
    * Gets the settings form route.
    *
