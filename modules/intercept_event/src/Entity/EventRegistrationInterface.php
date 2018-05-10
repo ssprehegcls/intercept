bc8ff6973cba2508e7a59dba65706a103f2320bf
Alan <alan@atendesigngroup.com>
2018-05-10 23:00:28 +0200

    RICHAPP-600: Changes to event registration entity from drupal console

--- modules/intercept_event/src/Entity/EventRegistrationInterface.php
+++ modules/intercept_event/src/Entity/EventRegistrationInterface.php
@@ -13,27 +13,6 @@ use Drupal\user\EntityOwnerInterface;
  */
 interface EventRegistrationInterface extends ContentEntityInterface, EntityChangedInterface, EntityOwnerInterface {
 
-  // Add get/set methods for your configuration properties here.
-
-  /**
-   * Gets the Event Registration name.
-   *
-   * @return string
-   *   Name of the Event Registration.
-   */
-  public function getName();
-
-  /**
-   * Sets the Event Registration name.
-   *
-   * @param string $name
-   *   The Event Registration name.
-   *
-   * @return \Drupal\intercept_event\Entity\EventRegistrationInterface
-   *   The called Event Registration entity.
-   */
-  public function setName($name);
-
   /**
    * Gets the Event Registration creation timestamp.
    *
@@ -52,26 +31,5 @@ interface EventRegistrationInterface extends ContentEntityInterface, EntityChang
    *   The called Event Registration entity.
    */
   public function setCreatedTime($timestamp);
-
-  /**
-   * Returns the Event Registration published status indicator.
-   *
-   * Unpublished Event Registration are only visible to restricted users.
-   *
-   * @return bool
-   *   TRUE if the Event Registration is published.
-   */
-  public function isPublished();
-
-  /**
-   * Sets the published status of a Event Registration.
-   *
-   * @param bool $published
-   *   TRUE to set this Event Registration to published, FALSE to set it to unpublished.
-   *
-   * @return \Drupal\intercept_event\Entity\EventRegistrationInterface
-   *   The called Event Registration entity.
-   */
-  public function setPublished($published);
 
 }
