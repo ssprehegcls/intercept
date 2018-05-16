6fbf03f129d910efb176873acbbeea1ce8486d7b
John Ferris <john@atendesigngroup.com>
2018-05-16 10:28:53 -0600

    RICHAPP-603: Fix multiple JSS instance issue and add a withIntercept higher order component.

--- modules/intercept_core/js/src/interceptClient.js
+++ modules/intercept_core/js/src/interceptClient.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import intercept from 'intercept-client';
 import drupalSettings from 'drupalSettings';
 import thunk from 'redux-thunk';
+import * as mui from './lib/mui';
 import * as select from './lib/select';
 import * as utils from './lib/utils';
 import history from './lib/history';
@@ -19,6 +20,6 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 /* eslint-enable */
 
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
-const interceptClient = Object.assign({}, intercept, { store, select, consumer, history, utils });
+const interceptClient = Object.assign({}, intercept, { mui, store, select, consumer, history, utils });
 window.interceptClient = interceptClient;
 export default interceptClient;
