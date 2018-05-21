ebcba811932ff0abb3857f299e3a5a6a44682c46
Alan <alan@atendesigngroup.com>
2018-05-21 18:53:25 +0200

    combined event entity changes

---
+++ modules/intercept_event/src/EventPermissionProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\intercept_event;
+
+
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\entity\EntityPermissionProvider;
+
+class EventPermissionProvider extends EntityPermissionProvider {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildPermissions(EntityTypeInterface $entity_type) {
+    $entity_type_id = $entity_type->id();
+    $has_owner = $entity_type->entityClassImplements(EntityOwnerInterface::class);
+    $plural_label = $entity_type->getPluralLabel();
+
+    $permissions = parent::buildPermissions($entity_type);
+    $permissions["view referenced user {$entity_type_id}"] = [
+      'title' => $this->t('View referenced user\'s @type', ['@type' => $plural_label]),
+    ];
+    $permissions["update referenced user {$entity_type_id}"] = [
+      'title' => $this->t('Update referenced user\'s @type', ['@type' => $plural_label]),
+    ];
+
+    return $this->processPermissions($permissions, $entity_type);
+  }
+
+}
