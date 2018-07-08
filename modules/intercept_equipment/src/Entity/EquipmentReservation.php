1f02dc211545cf7dd2599d6eaf1c33f3618a56fa
Alan <alan@atendesigngroup.com>
2018-07-08 17:18:30 +0200

    Simplified room reservation and equipment entity definitions and extended base reservation

--- modules/intercept_equipment/src/Entity/EquipmentReservation.php
+++ modules/intercept_equipment/src/Entity/EquipmentReservation.php
@@ -4,11 +4,11 @@ namespace Drupal\intercept_equipment\Entity;
 
 use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Field\BaseFieldDefinition;
-use Drupal\Core\Entity\RevisionableContentEntityBase;
 use Drupal\Core\Entity\RevisionableInterface;
 use Drupal\Core\Entity\EntityChangedTrait;
 use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\intercept_core\Entity\ReservationBase;
 use Drupal\intercept_equipment\Field\Computed\EntityReferenceFieldItemList;
 use Drupal\intercept_equipment\Field\Computed\FileFieldItemList;
 use Drupal\intercept_equipment\Field\Computed\MethodItemList;
@@ -74,235 +74,13 @@ use Drupal\user\UserInterface;
  *   field_ui_base_route = "equipment_reservation.settings"
  * )
  */
-class EquipmentReservation extends RevisionableContentEntityBase implements EquipmentReservationInterface {
-
-  use EntityChangedTrait;
-
-  use StringTranslationTrait;
-
+class EquipmentReservation extends ReservationBase implements EquipmentReservationInterface {
 
   /**
    * {@inheritdoc}
    */
-  public static function preCreate(EntityStorageInterface $storage_controller, array &$values) {
-    parent::preCreate($storage_controller, $values);
-    $values += [
-      'author' => \Drupal::currentUser()->id(),
-    ];
-  }
-
-  public function label() {
-    $dates = $this->get('field_dates')->first();
-    if (!$dates || !$dates->get('value') || !$dates->get('end_value')) {
-      return '';
-    }
-    $values = [];
-    if ($from_date = $dates->get('value')->getDateTime()) {
-      $values['@date'] = $from_date->format('F n, Y');
-      $values['@time_start'] = $from_date->format('h:i A');
-    }
-    if ($to_date = $dates->get('end_value')->getDateTime()) {
-      $values['@time_end'] = $to_date->format('h:i A');
-    }
-    return !empty($values) ? $this->t('@date from @time_start to @time_end', $values) : '';
-  }
-
-  public function location() {
-    return $this->t('At @location @equipment', [
-      '@location' => $this->get('equipment_location')->entity ? $this->get('equipment_location')->entity->label() : '',
-      '@equipment' => $this->get('field_equipment')->entity ? $this->get('field_equipment')->entity->label() : '',
-    ]);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function urlRouteParameters($rel) {
-    $uri_route_parameters = parent::urlRouteParameters($rel);
-
-    if ($rel === 'revision-revert-form' && $this instanceof RevisionableInterface) {
-      $uri_route_parameters[$this->getEntityTypeId() . '_revision'] = $this->getRevisionId();
-    }
-    if ($rel === 'revision-delete-form' && $this instanceof RevisionableInterface) {
-      $uri_route_parameters[$this->getEntityTypeId() . '_revision'] = $this->getRevisionId();
-    }
-
-    return $uri_route_parameters;
-  }
-
-
-  /**
-   * {@inheritdoc}
-   */
-  public function preSave(EntityStorageInterface $storage) {
-    if (!empty($this->original) && !$this->original->get('field_status')->equals($this->get('field_status'))){
-      $this->setNewRevision(TRUE);
-    }
-    parent::preSave($storage);
-
-    foreach (array_keys($this->getTranslationLanguages()) as $langcode) {
-      $translation = $this->getTranslation($langcode);
-
-      // If no owner has been set explicitly, make the anonymous user the owner.
-      if (!$translation->getOwner()) {
-        $translation->setOwnerId(0);
-      }
-    }
-
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function preSaveRevision(EntityStorageInterface $storage, \stdClass $record) {
-    parent::preSaveRevision($storage, $record);
-
-    $is_new_revision = $this->isNewRevision();
-      // @see \Drupal\media\Entity\Media::preSaveRevision()
-    if (!$is_new_revision && isset($this->original) && empty($record->revision_log_message)) {
-      $record->revision_log_message = $this->original->revision_log_message->value;
-    }
-
-    if ($is_new_revision) {
-      $record->revision_created = \Drupal::time()->getRequestTime();
-      $record->revision_user =  \Drupal::currentUser()->id();
-    }
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getCreatedTime() {
-    return $this->get('created')->value;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setCreatedTime($timestamp) {
-    $this->set('created', $timestamp);
-    return $this;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getOwner() {
-    return $this->get('author')->entity;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getOwnerId() {
-    return $this->get('author')->target_id;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setOwnerId($uid) {
-    $this->set('author', $uid);
-    return $this;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setOwner(UserInterface $account) {
-    $this->set('author', $account->id());
-    return $this;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function isPublished() {
-    return (bool) $this->getEntityKey('status');
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setPublished($published) {
-    $this->set('status', $published ? TRUE : FALSE);
-    return $this;
-  }
-
-  public function title() {
-    return $this->label();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
-    $fields = parent::baseFieldDefinitions($entity_type);
-
-    $fields['title'] = BaseFieldDefinition::create('string')
-      ->setLabel(t('Title'))
-      ->setComputed(TRUE)
-      ->setClass(MethodItemList::class)
-      ->setSetting('method', 'label')
-      ->setReadOnly(TRUE);
-
-    $fields['author'] = BaseFieldDefinition::create('entity_reference')
-      ->setLabel(t('Authored by'))
-      ->setDescription(t('The user ID of author of the equipment reservation entity.'))
-      ->setRevisionable(TRUE)
-      ->setSetting('target_type', 'user')
-      ->setSetting('handler', 'default')
-      ->setTranslatable(TRUE)
-      ->setDisplayOptions('view', [
-        'label' => 'hidden',
-        'type' => 'author',
-        'weight' => 0,
-      ])
-      ->setDisplayOptions('form', [
-        'type' => 'entity_reference_autocomplete',
-        'weight' => 5,
-        'settings' => [
-          'match_operator' => 'CONTAINS',
-          'size' => '60',
-          'autocomplete_type' => 'tags',
-          'placeholder' => '',
-        ],
-      ])
-      ->setDisplayConfigurable('form', TRUE)
-      ->setDisplayConfigurable('view', TRUE);
-
-    $fields['image'] = BaseFieldDefinition::create('image')
-      ->setLabel(t('Image'))
-      ->setDescription(t('The related equipment entity\'s image.'))
-      ->setComputed(TRUE)
-      ->setClass(FileFieldItemList::class)
-      ->setDisplayConfigurable('form', TRUE)
-      ->setDisplayConfigurable('view', TRUE)
-      ->setSetting('target_fields', ['field_equipment', 'image_primary', 'field_media_image'])
-      ->setReadOnly(TRUE);
-
-    $fields['status'] = BaseFieldDefinition::create('boolean')
-      ->setLabel(t('Publishing status'))
-      ->setDescription(t('A boolean indicating whether the Equipment reservation is published.'))
-      ->setRevisionable(TRUE)
-      ->setDefaultValue(TRUE);
-
-    $fields['created'] = BaseFieldDefinition::create('created')
-      ->setLabel(t('Created'))
-      ->setDescription(t('The time that the entity was created.'));
-
-    $fields['changed'] = BaseFieldDefinition::create('changed')
-      ->setLabel(t('Changed'))
-      ->setDescription(t('The time that the entity was last edited.'));
-
-    $fields['revision_translation_affected'] = BaseFieldDefinition::create('boolean')
-      ->setLabel(t('Revision translation affected'))
-      ->setDescription(t('Indicates if the last edit of a translation belongs to current revision.'))
-      ->setReadOnly(TRUE)
-      ->setRevisionable(TRUE)
-      ->setTranslatable(TRUE);
-
-    return $fields;
+  public static function reservationType() {
+    return 'equipment';
   }
 
 }
--- modules/intercept_room_reservation/src/Entity/RoomReservation.php
+++ modules/intercept_room_reservation/src/Entity/RoomReservation.php
@@ -4,11 +4,11 @@ namespace Drupal\intercept_room_reservation\Entity;
 
 use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Field\BaseFieldDefinition;
-use Drupal\Core\Entity\RevisionableContentEntityBase;
 use Drupal\Core\Entity\RevisionableInterface;
 use Drupal\Core\Entity\EntityChangedTrait;
 use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\intercept_core\Entity\ReservationBase;
 use Drupal\intercept_room_reservation\Field\Computed\EntityReferenceFieldItemList;
 use Drupal\intercept_room_reservation\Field\Computed\FileFieldItemList;
 use Drupal\intercept_room_reservation\Field\Computed\MethodItemList;
@@ -40,7 +40,7 @@ use Drupal\user\UserInterface;
  *       "decline" = "Drupal\intercept_room_reservation\Form\RoomReservationUpdateStatusForm",
  *     },
  *     "access" = "Drupal\intercept_room_reservation\RoomReservationAccessControlHandler",
- *     "permission_provider" = "Drupal\intercept_room_reservation\RoomReservationPermissionsProvider",
+ *     "permission_provider" = "Drupal\intercept_core\ReservationPermissionsProvider",
  *     "route_provider" = {
  *       "html" = "Drupal\intercept_room_reservation\RoomReservationHtmlRouteProvider",
  *       "revision" = "Drupal\intercept_room_reservation\RoomReservationRevisionRouteProvider",
@@ -80,60 +80,13 @@ use Drupal\user\UserInterface;
  *   field_ui_base_route = "room_reservation.settings"
  * )
  */
-class RoomReservation extends RevisionableContentEntityBase implements RoomReservationInterface {
-
-  use EntityChangedTrait;
-
-  use StringTranslationTrait;
-
+class RoomReservation extends ReservationBase implements RoomReservationInterface {
 
   /**
    * {@inheritdoc}
    */
-  public static function preCreate(EntityStorageInterface $storage_controller, array &$values) {
-    parent::preCreate($storage_controller, $values);
-    $values += [
-      'author' => \Drupal::currentUser()->id(),
-    ];
-  }
-
-  public function label() {
-    $dates = $this->get('field_dates')->first();
-    if (!$dates || !$dates->get('value') || !$dates->get('end_value')) {
-      return '';
-    }
-    $values = [];
-    if ($from_date = $dates->get('value')->getDateTime()) {
-      $values['@date'] = $from_date->format('F n, Y');
-      $values['@time_start'] = $from_date->format('h:i A');
-    }
-    if ($to_date = $dates->get('end_value')->getDateTime()) {
-      $values['@time_end'] = $to_date->format('h:i A');
-    }
-    return !empty($values) ? $this->t('@date from @time_start to @time_end', $values) : '';
-  }
-
-  public function location() {
-    return $this->t('At @location @room', [
-      '@location' => $this->get('room_location')->entity ? $this->get('room_location')->entity->label() : '',
-      '@room' => $this->get('field_room')->entity ? $this->get('field_room')->entity->label() : '',
-    ]);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function urlRouteParameters($rel) {
-    $uri_route_parameters = parent::urlRouteParameters($rel);
-
-    if ($rel === 'revision-revert-form' && $this instanceof RevisionableInterface) {
-      $uri_route_parameters[$this->getEntityTypeId() . '_revision'] = $this->getRevisionId();
-    }
-    if ($rel === 'revision-delete-form' && $this instanceof RevisionableInterface) {
-      $uri_route_parameters[$this->getEntityTypeId() . '_revision'] = $this->getRevisionId();
-    }
-
-    return $uri_route_parameters;
+  public static function reservationType() {
+    return 'room';
   }
 
   public function cancel() {
@@ -155,162 +108,17 @@ class RoomReservation extends RevisionableContentEntityBase implements RoomReser
     return $this;
   }
 
-
-  /**
-   * {@inheritdoc}
-   */
-  public function preSave(EntityStorageInterface $storage) {
-    if (!empty($this->original) && !$this->original->get('field_status')->equals($this->get('field_status'))){
-      $this->setNewRevision(TRUE);
-    }
-    parent::preSave($storage);
-
-    foreach (array_keys($this->getTranslationLanguages()) as $langcode) {
-      $translation = $this->getTranslation($langcode);
-
-      // If no owner has been set explicitly, make the anonymous user the owner.
-      if (!$translation->getOwner()) {
-        $translation->setOwnerId(0);
-      }
-    }
-
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function preSaveRevision(EntityStorageInterface $storage, \stdClass $record) {
-    parent::preSaveRevision($storage, $record);
-
-    $is_new_revision = $this->isNewRevision();
-      // @see \Drupal\media\Entity\Media::preSaveRevision()
-    if (!$is_new_revision && isset($this->original) && empty($record->revision_log_message)) {
-      $record->revision_log_message = $this->original->revision_log_message->value;
-    }
-
-    if ($is_new_revision) {
-      $record->revision_created = \Drupal::time()->getRequestTime();
-      $record->revision_user =  \Drupal::currentUser()->id();
-    }
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getCreatedTime() {
-    return $this->get('created')->value;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setCreatedTime($timestamp) {
-    $this->set('created', $timestamp);
-    return $this;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getOwner() {
-    return $this->get('author')->entity;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getOwnerId() {
-    return $this->get('author')->target_id;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setOwnerId($uid) {
-    $this->set('author', $uid);
-    return $this;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setOwner(UserInterface $account) {
-    $this->set('author', $account->id());
-    return $this;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function isPublished() {
-    return (bool) $this->getEntityKey('status');
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setPublished($published) {
-    $this->set('status', $published ? TRUE : FALSE);
-    return $this;
-  }
-
-  public function title() {
-    return $this->label();
-  }
-
   /**
    * {@inheritdoc}
    */
   public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
     $fields = parent::baseFieldDefinitions($entity_type);
 
-    $fields['title'] = BaseFieldDefinition::create('string')
-      ->setLabel(t('Title'))
-      ->setComputed(TRUE)
-      ->setClass(MethodItemList::class)
-      ->setSetting('method', 'label')
-      ->setReadOnly(TRUE);
-
     $fields['location'] = BaseFieldDefinition::create('string')
       ->setLabel(t('Title'))
       ->setComputed(TRUE)
       ->setClass(MethodItemList::class)
       ->setSetting('method', 'location')
-      ->setReadOnly(TRUE);
-
-    $fields['author'] = BaseFieldDefinition::create('entity_reference')
-      ->setLabel(t('Authored by'))
-      ->setDescription(t('The user ID of author of the Room reservation entity.'))
-      ->setRevisionable(TRUE)
-      ->setSetting('target_type', 'user')
-      ->setSetting('handler', 'default')
-      ->setTranslatable(TRUE)
-      ->setDisplayOptions('view', [
-        'label' => 'hidden',
-        'type' => 'author',
-        'weight' => 0,
-      ])
-      ->setDisplayOptions('form', [
-        'type' => 'entity_reference_autocomplete',
-        'weight' => 5,
-        'settings' => [
-          'match_operator' => 'CONTAINS',
-          'size' => '60',
-          'autocomplete_type' => 'tags',
-          'placeholder' => '',
-        ],
-      ])
-      ->setDisplayConfigurable('form', TRUE)
-      ->setDisplayConfigurable('view', TRUE);
-
-    $fields['image'] = BaseFieldDefinition::create('image')
-      ->setLabel(t('Image'))
-      ->setDescription(t('The related room entity\'s image.'))
-      ->setComputed(TRUE)
-      ->setClass(FileFieldItemList::class)
-      ->setDisplayConfigurable('form', TRUE)
-      ->setDisplayConfigurable('view', TRUE)
-      ->setSetting('target_fields', ['field_room', 'image_primary', 'field_media_image'])
       ->setReadOnly(TRUE);
 
     $fields['room_location'] = BaseFieldDefinition::create('entity_reference')
@@ -323,27 +131,6 @@ class RoomReservation extends RevisionableContentEntityBase implements RoomReser
       ->setTargetEntityTypeId('node')->setTargetBundle('location')
       ->setSetting('target_fields', ['field_room', 'field_location'])
       ->setReadOnly(TRUE);
-
-    $fields['status'] = BaseFieldDefinition::create('boolean')
-      ->setLabel(t('Publishing status'))
-      ->setDescription(t('A boolean indicating whether the Room reservation is published.'))
-      ->setRevisionable(TRUE)
-      ->setDefaultValue(TRUE);
-
-    $fields['created'] = BaseFieldDefinition::create('created')
-      ->setLabel(t('Created'))
-      ->setDescription(t('The time that the entity was created.'));
-
-    $fields['changed'] = BaseFieldDefinition::create('changed')
-      ->setLabel(t('Changed'))
-      ->setDescription(t('The time that the entity was last edited.'));
-
-    $fields['revision_translation_affected'] = BaseFieldDefinition::create('boolean')
-      ->setLabel(t('Revision translation affected'))
-      ->setDescription(t('Indicates if the last edit of a translation belongs to current revision.'))
-      ->setReadOnly(TRUE)
-      ->setRevisionable(TRUE)
-      ->setTranslatable(TRUE);
 
     return $fields;
   }
