07b36c57fa62f1503a474c62a0328a941273e208
Mark Jarrell <mjarrell@richlandlibrary.com>
2018-07-10 13:45:04 -0400

    Fixes name conflict on room reservations views.

--- modules/intercept_room_reservation/src/Controller/ManagementController.php
+++ modules/intercept_room_reservation/src/Controller/ManagementController.php
@@ -20,7 +20,7 @@ class ManagementController extends ManagementControllerBase {
   public function viewStaffRoomReservations(AccountInterface $user, Request $request) {
     return [
       '#type' => 'view',
-      '#name' => 'room_reservations',
+      '#name' => 'room_reservations_admin',
       '#display_id' => 'embed', 
     ];
   }
