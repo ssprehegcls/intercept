d78d2407d8c4d100b5536026b435eb4a3df0c3a1
Alan <alan@atendesigngroup.com>
2018-07-18 17:50:49 -0600

    Adds YAML support to API for better organizing of info

---
+++ modules/intercept_event/src/Form/EventAttendanceScanLookupForm.php
@@ -0,0 +1,217 @@
+<?php
+
+namespace Drupal\intercept_event\Form;
+
+use Drupal\Component\Datetime\TimeInterface;
+use Drupal\Core\Entity\ContentEntityForm;
+use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\externalauth\ExternalAuth;
+use Drupal\polaris\Client;
+use Drupal\user\UserInterface;
+use Drupal\user\UserStorage;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Form controller for Event Attendance edit forms.
+ *
+ * @ingroup intercept_event
+ */
+class EventAttendanceScanLookupForm extends EventAttendanceScanFormBase {
+
+  /**
+   * @var Client
+   */
+  protected $client;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(EntityManagerInterface $entity_manager, EntityTypeBundleInfoInterface $entity_type_bundle_info = NULL, TimeInterface $time = NULL, ExternalAuth $external_auth, Client $client) {
+    parent::__construct($entity_manager, $entity_type_bundle_info, $time, $external_auth);
+    $this->client = $client;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity.manager'),
+      $container->get('entity_type.bundle.info'),
+      $container->get('datetime.time'),
+      $container->get('externalauth.externalauth'),
+      $container->get('polaris.client')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function instructionsHeaderText() {
+    return $this->t('Library card lookup.');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function instructionsText() {
+    return $this->t('Enter your first name, last name and email to find your library card.');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildForm($form, $form_state);
+    $form['barcode']['#access'] = FALSE;
+
+    $results = $form_state->getTemporaryValue('results');
+    if (isset($results)) {
+      $this->buildResultsForm($form, $form_state);
+    }
+    else {
+      $this->buildSearchForm($form, $form_state);
+    }
+    return $form;
+  }
+
+  protected function buildResultsForm(array &$form, FormStateInterface $form_state) {
+    $form['results'] = [
+      '#type' => 'tableselect',
+      '#multiple' => FALSE,
+      '#header' => [
+        'name' => $this->t('Name'),
+        'barcode' => $this->t('Barcode'),
+        'email' => $this->t('Email'),
+      ],
+      '#options' => [],
+      '#empty' => $this->t('No results found'),
+    ];
+    foreach ($form_state->getTemporaryValue('results') as $result) {
+      $patron = $this->client->patron->get($result->Barcode);
+      $form['results']['#options'][$result->Barcode] = [
+        'name' => $this->formatName($result->PatronFirstLastName),
+        'barcode' => $this->obfuscateBarcode($result->Barcode),
+        'email' => $this->obfuscateEmail($patron->data()->EmailAddress),
+      ];
+    }
+    $form['actions']['submit']['#value'] = $this->t('Sign me in');
+
+    $form['cancel'] = $this->cancelButton();
+
+    $form['retry'] = [
+      '#type' => 'link',
+      '#title' => $this->t('Try another search'),
+      '#url' => \Drupal\Core\Url::fromRoute('entity.node.scan_lookup', [
+        'node' => $this->event()->id(),
+      ]),
+    ];
+  }
+
+  protected function buildSearchForm(array &$form, FormStateInterface $form_state) {
+    $form['actions']['#access'] = FALSE;
+
+    $form['first_name'] = [
+      '#title' => $this->t('First name'),
+      '#type' => 'textfield',
+    ];
+
+    $form['middle_name'] = [
+      '#title' => $this->t('Middle name'),
+      '#type' => 'textfield',
+    ];
+
+    $form['last_name'] = [
+      '#title' => $this->t('Last name'),
+      '#type' => 'textfield',
+    ];
+
+    $form['email'] = [
+      '#title' => $this->t('Email'),
+      '#type' => 'textfield',
+    ];
+
+    $form['search'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Search'),
+      '#validate' => ['::search'],
+    ];
+  }
+
+  public function search(array $form, FormStateInterface $form_state) {
+    $values = $form_state->cleanValues()->getValues();
+    $form_state->setTemporaryValue('results', []);
+    if (empty($values['last_name']) && empty($values['email'])) {
+      $form_state->setErrorByName('last_name', $this->t('Please provide either a last name or email.'));
+      return;
+    }
+    $query = [
+      'PATNF' => $values['first_name'] . '*',
+      'PATNL' => $values['last_name'] . '*'
+    ];
+    if (!empty($values['email'])) { 
+      $query['EM'] = 'mjarrell@richlandlibrary.com';
+    }
+    $results = $this->client->patron->searchAnd($query);
+    if (!empty($results->PatronSearchRows)) {
+      $form_state->setTemporaryValue('results', $results->PatronSearchRows);
+    }
+    $form_state->setRebuild();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $values = $form_state->cleanValues()->getValues();
+    if (empty($values['results'])) {
+      $form_state->setErrorByName('results', $this->t('You must select an account to sign in.'));
+      return;
+    }
+    $form_state->setValue('barcode', $values['results']);
+
+    $user = $this->createAttendee($values['results']);
+    if ($this->attendanceExists($user->id())) {
+      $this->setBarcodeError(static::ATTENDANCE_EXISTS_MESSAGE, $form, $form_state);
+    }
+    else {
+      $this->populateAttendance($form, $form_state, $user);
+    }
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+    $this->redirectToBaseForm($form_state);
+  }
+
+  protected function obfuscateEmail($email) {
+    if (empty($email)) {
+      return '';
+    }
+    $pos = strpos($email, '@');
+    return substr_replace($email, str_repeat('*', $pos - 1), 1, $pos - 1);
+  }
+
+  protected function obfuscateBarcode($barcode) {
+    if (empty($barcode)) {
+      return '';
+    }
+    $replace = str_repeat('*', strlen($barcode) - 4);
+    return substr_replace($barcode, $replace, 0, strlen($barcode) - 4);
+  } 
+
+  protected function formatName($name) {
+    if (empty($name)) {
+      return '';
+    }
+    $name = array_reverse(explode(',', $name));
+    $name = array_map('trim', $name);
+    return implode(' ', $name);
+  }
+}
