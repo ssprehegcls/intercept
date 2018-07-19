a081b0fac2bc32d3fb4e6c556674e81fc629f9ab
Alan <alan@atendesigngroup.com>
2018-07-18 18:55:17 -0600

    Adds preview functionality for templates and finishes other template functionality

--- modules/intercept_event/src/EventManager.php
+++ modules/intercept_event/src/EventManager.php
@@ -4,12 +4,16 @@ namespace Drupal\intercept_event;
 
 use Drupal\Component\Serialization\Json;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Link;
 use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\node\NodeInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class EventManager implements EventManagerInterface {
+
+  use StringTranslationTrait;
 
   /**
    * Constructs a new EventManager object.
@@ -45,11 +49,47 @@ class EventManager implements EventManagerInterface {
     }
   }
 
-  public function addFromTemplate(\Drupal\node\NodeInterface $node) {
+  /**
+   * Create an event node clone with certain changes.
+   *
+   * @param NodeInterface $node
+   * @return NodeInterface
+   */
+  private function cloneify(\Drupal\node\NodeInterface $node) {
     $new_node = $node->createDuplicate();
     $new_node->set('field_event_is_template', 0);
+    foreach (['vid', 'field_date_time', 'field_event_register_period'] as $field) {
+      $new_node->set($field, NULL);
+    }
+
     $new_node->setOwnerId(\Drupal::currentUser()->id());
-    $form = \Drupal::service('entity.form_builder')->getForm($new_node);
+    return $new_node;
+  }
+
+  /**
+   * View a node cloned from a template.
+   *
+   * @param NodeInterface $node
+   * @return array
+   */
+  public function previewFromTemplate(\Drupal\node\NodeInterface $node) {
+    $new_node = $this->cloneify($node);
+    drupal_set_message($this->t('This is a preview. @use_link.', [
+      '@use_link' => Link::createFromRoute('Use this template', 'entity.node.template', [
+        'node' => $node->id(),
+      ])->toString(),
+    ]), 'warning');
+    return $this->entityTypeManager->getViewBuilder('node')->view($new_node, 'full');
+  }
+
+  /**
+   * Edit a node cloned from a template.
+   *
+   * @param NodeInterface $node
+   * @return mixed
+   */
+  public function addFromTemplate(\Drupal\node\NodeInterface $node) {
+    $form = \Drupal::service('entity.form_builder')->getForm($this->cloneify($node));
     return $form;
   }
 
