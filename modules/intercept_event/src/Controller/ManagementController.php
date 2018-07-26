d9c866002af78ace368260352a7c9a43a30ea77a
Alan <alan@atendesigngroup.com>
2018-07-27 00:09:57 +0200

    Adds event link to events page and reuses code for dashboard

--- modules/intercept_event/src/Controller/ManagementController.php
+++ modules/intercept_event/src/Controller/ManagementController.php
@@ -15,11 +15,19 @@ class ManagementController extends ManagementControllerBase {
     if ($page_name == 'staff_default' || $page_name == 'admin_default') {
       $type = $page_name == 'staff_default' ? 'staff' : 'admin';
       $route = "intercept_event.management.{$type}_event_templates";
-      $build['links']['event'] = $this->getButton('Create an event', $route, [
-        'user' => $this->currentUser()->id(),
-      ]);
+      $build['links']['event'] = $this->getCreateEventButton($type);
       $build['links']['events_all'] = $this->getButton('View All Events', 'intercept_event.events_all');
     }
+  }
+
+  /**
+   * Helper function to create a link to the template page for creating events.
+   */
+  private function getCreateEventButton($type = 'staff') {
+    $route = "intercept_event.management.{$type}_event_templates";
+    return $this->getButton('Create an Event', $route, [
+      'user' => $this->currentUser()->id(),
+    ]);
   }
 
   public function viewAdminEventConfiguration(AccountInterface $user, Request $request) {
@@ -50,16 +58,20 @@ class ManagementController extends ManagementControllerBase {
     ];
   }
 
-  public function viewStaffEvents(AccountInterface $user, Request $request) {
+  public function viewStaffEvents(AccountInterface $user, Request $request, $is_admin = FALSE) {
     return [
-      '#type' => 'view',
-      '#name' => 'intercept_events',
-      '#display_id' => 'embed',
+      'title' => $this->title('Events'),
+      'event_create' => $this->getCreateEventButton($is_admin ? 'admin' : 'staff'),
+      'content' => [
+        '#type' => 'view',
+        '#name' => 'intercept_events',
+        '#display_id' => 'embed',
+      ],
     ];
   }
 
   public function viewAdminEvents(AccountInterface $user, Request $request) {
-    return $this->viewStaffEvents($user, $request);
+    return $this->viewStaffEvents($user, $request, TRUE);
   }
 
   public function viewStaffEventTemplates(AccountInterface $user, Request $request) {
