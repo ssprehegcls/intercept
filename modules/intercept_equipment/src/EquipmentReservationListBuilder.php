ea43e6ff58e441447a822ce4f3cfeb3f31a0ec60
Mark Jarrell <mjarrell@richlandlibrary.com>
2018-07-28 16:32:14 -0400

    Updates reservation list builders to not show the dates/times in UTC

--- modules/intercept_equipment/src/EquipmentReservationListBuilder.php
+++ modules/intercept_equipment/src/EquipmentReservationListBuilder.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\intercept_equipment;
 
+use Drupal\Core\Datetime\DrupalDateTime;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityListBuilder;
 use Drupal\Core\Link;
@@ -17,7 +18,7 @@ class EquipmentReservationListBuilder extends EntityListBuilder {
    * {@inheritdoc}
    */
   public function buildHeader() {
-    $header['name'] = $this->t('Name');
+    $header['name'] = $this->t('Reservation');
     $header['equipment'] = $this->t('Equipment');
     $header['location'] = $this->t('Location');
     $header['user'] = $this->t('User');
@@ -28,8 +29,15 @@ class EquipmentReservationListBuilder extends EntityListBuilder {
    * {@inheritdoc}
    */
   public function buildRow(EntityInterface $entity) {
+    // Fix the dates to be non-UTC in the display.
+    // Dates/times of reservation
+    $reservation_dates = $entity->get('field_dates')->getValue();
+    $dateTime = new DrupalDateTime($reservation_dates[0]['value'], 'UTC');
+    $reservation_start_date = date('m-d-Y g:i A', $dateTime->getTimestamp());
+    $dateTime = new DrupalDateTime($reservation_dates[0]['end_value'], 'UTC');
+    $reservation_end_date = date('m-d-Y g:i A', $dateTime->getTimestamp());
     /* @var $entity \Drupal\intercept_equipment\Entity\EquipmentReservation */
-    $row['name'] = $entity->link();
+    $row['name'] = $entity->link($reservation_start_date . ' - ' . $reservation_end_date);
     $row['equipment'] = $this->getEntityLabel($entity->field_equipment->entity, $this->t('No equipment'));
     $row['location'] = $entity->get('field_location')->entity ? $entity->get('field_location')->entity->label() : '';
     $row['user'] = $this->getEntityLabel($entity->field_user->entity, $this->t('No user'));
--- modules/intercept_room_reservation/src/RoomReservationListBuilder.php
+++ modules/intercept_room_reservation/src/RoomReservationListBuilder.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\intercept_room_reservation;
 
+use Drupal\Core\Datetime\DrupalDateTime;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityListBuilder;
 use Drupal\Core\Link;
@@ -17,7 +18,7 @@ class RoomReservationListBuilder extends EntityListBuilder {
    * {@inheritdoc}
    */
   public function buildHeader() {
-    $header['name'] = $this->t('Name');
+    $header['name'] = $this->t('Reservation');
     $header['room'] = $this->t('Room');
     $header['location'] = $this->t('Location');
     $header['user'] = $this->t('User');
@@ -29,8 +30,15 @@ class RoomReservationListBuilder extends EntityListBuilder {
    * {@inheritdoc}
    */
   public function buildRow(EntityInterface $entity) {
+    // Fix the dates to be non-UTC in the display.
+    // Dates/times of reservation
+    $reservation_dates = $entity->get('field_dates')->getValue();
+    $dateTime = new DrupalDateTime($reservation_dates[0]['value'], 'UTC');
+    $reservation_start_date = date('m-d-Y g:i A', $dateTime->getTimestamp());
+    $dateTime = new DrupalDateTime($reservation_dates[0]['end_value'], 'UTC');
+    $reservation_end_date = date('m-d-Y g:i A', $dateTime->getTimestamp());
     /* @var $entity \Drupal\intercept_room_reservation\Entity\RoomReservation */
-    $row['name'] = $entity->link();
+    $row['name'] = $entity->link($reservation_start_date . ' - ' . $reservation_end_date);
     $row['room'] = $this->getEntityLabel($entity->field_room->entity, $this->t('No room'));
     $row['location'] = $entity->get('location')->entity ? $entity->get('location')->entity->label() : '';
     $row['user'] = $this->getEntityLabel($entity->field_user->entity, $this->t('No user'));
