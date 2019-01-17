b66a586abeb45db2393272df9e48ade603a1bb1b
John Ferris <john@atendesigngroup.com>
2019-01-17 21:00:20 +0900

    Fix DST issue with date display

--- modules/intercept_core/js/src/lib/utils.js
+++ modules/intercept_core/js/src/lib/utils.js
@@ -1,21 +1,23 @@
-import moment from 'moment';
-import drupalSettings from 'drupalSettings';
-import get from 'lodash/get';
-import intersection from 'lodash/intersection';
+import moment from "moment";
+import drupalSettings from "drupalSettings";
+import get from "lodash/get";
+import intersection from "lodash/intersection";
 
 //
 // User getters
 //
-export const getUserUtcOffset = () => get(drupalSettings, 'intercept.user.utc_offset');
-export const getUserTimezone = () => get(drupalSettings, 'intercept.user.timezone');
-export const getUserName = () => get(drupalSettings, 'intercept.user.name');
-export const getUserUid = () => get(drupalSettings, 'intercept.user.id');
-export const getUserUuid = () => get(drupalSettings, 'intercept.user.uuid');
-export const getUserRoles = () => get(drupalSettings, 'intercept.user.roles');
+export const getUserUtcOffset = () =>
+  get(drupalSettings, "intercept.user.utc_offset");
+export const getUserTimezone = () =>
+  get(drupalSettings, "intercept.user.timezone");
+export const getUserName = () => get(drupalSettings, "intercept.user.name");
+export const getUserUid = () => get(drupalSettings, "intercept.user.id");
+export const getUserUuid = () => get(drupalSettings, "intercept.user.uuid");
+export const getUserRoles = () => get(drupalSettings, "intercept.user.roles");
 export const getUserStartOfDay = () =>
   moment()
     .tz(getUserTimezone())
-    .startOf('day')
+    .startOf("day")
     .toDate();
 export const getUserTimeNow = () =>
   moment()
@@ -37,7 +39,7 @@ export const newUserDate = (date = new Date()) =>
     .toDate();
 
 // Make sure the current value is a valid date object.
-export const ensureDate = (date) => {
+export const ensureDate = date => {
   if (date instanceof moment) {
     return date.toDate();
   }
@@ -52,65 +54,65 @@ export const getDateFromTime = (time, date) =>
     .tz(date, getUserTimezone())
     .hour(time.slice(0, 2))
     .minute(time.slice(2))
-    .startOf('minute')
+    .startOf("minute")
     .toDate();
 
 export const getTimeFromDate = date =>
   moment(date)
     .tz(getUserTimezone())
-    .format('HHmm');
+    .format("HHmm");
 
 // Normalize a date object to a single day. Used to compare days for different dates.
 // export const getDayTimeStamp = date => ensureDate(date).setHours(0, 0, 0, 0);
 export const getDayTimeStamp = date =>
   moment(date)
     .tz(getUserTimezone())
-    .startOf('day')
-    .format('YYYY-MM-DD');
+    .startOf("day")
+    .format("YYYY-MM-DD");
 
 export const getDateFromDayTimeStamp = timestamp =>
-  moment.tz(timestamp, 'YYYY-MM-DD', getUserTimezone()).toDate();
+  moment.tz(timestamp, "YYYY-MM-DD", getUserTimezone()).toDate();
 
 // Get a formatted date string.
-export const getDayDisplay = (date) => {
+export const getDayDisplay = date => {
   const d = getDayTimeStamp(date);
   const today = moment()
     .tz(getUserTimezone())
-    .startOf('day');
-  const tomorrow = today.clone().add(1, 'days');
+    .startOf("day");
+  const tomorrow = today.clone().add(1, "days");
 
   // Today
   if (d === getDayTimeStamp(today)) {
-    return 'Today';
+    return "Today";
   }
   // Tommorrow
   if (d === getDayTimeStamp(tomorrow)) {
-    return 'Tomorrow';
+    return "Tomorrow";
   }
   // Friday, October 20, 2017
   return moment(date)
     .tz(getUserTimezone())
-    .format('dddd, MMMM D, YYYY');
+    .format("dddd, MMMM D, YYYY");
 };
 
 // Get a formatted short date string.
-export const getDateDisplay = (date) => {
+export const getDateDisplay = date => {
   const d = getDayTimeStamp(date);
 
   // Today
   if (d === getDayTimeStamp(new Date())) {
-    return 'Today';
+    return "Today";
   }
   // Tommorrow
   const tomorrow = new Date();
   tomorrow.setDate(tomorrow.getDate() + 1);
   if (d === getDayTimeStamp(tomorrow)) {
-    return 'Tomorrow';
+    return "Tomorrow";
   }
   // Friday, October 20, 2017
   return moment(date)
-    .utcOffset(getUserUtcOffset())
-    .format('M/D/YYYY');
+    .tz(getUserTimezone())
+    .format("M/D/YYYY");
 };
 
 // Get a formatted time string
@@ -119,16 +121,16 @@ export const getTimeDisplay = date =>
   // 2p.m.
   moment(date)
     .tz(getUserTimezone())
-    .format('h:mm a')
-    .replace('m', '.m.');
+    .format("h:mm a")
+    .replace("m", ".m.");
 
 // Get a formatted time string
 //   Example: '07/13/79 2p.m. to 4p.m.'
 export const getDateTimespanDisplay = ({ date, start, end }) =>
   // const { date, start, end } = this.props.values;
-  `${getDateDisplay(date)} ${getTimeDisplay(getDateFromTime(start))} to ${getTimeDisplay(
-    getDateFromTime(end),
-  )}`;
+  `${getDateDisplay(date)} ${getTimeDisplay(
+    getDateFromTime(start)
+  )} to ${getTimeDisplay(getDateFromTime(end))}`;
 
 // Gets the duration in minutes of an event based on start and end time.
 export const getDurationInMinutes = (start, end) =>
@@ -139,13 +141,19 @@ export const getDurationInMinutes = (start, end) =>
 export const dateToDrupal = date =>
   ensureDate(date)
     .toISOString()
-    .replace('.000Z', '')
-    .replace('.999Z', ''); // @todo Replace with regex
+    .replace(".000Z", "")
+    .replace(".999Z", ""); // @todo Replace with regex
 
 // Converts a Drupal compatible string to a Date object.
-export const dateFromDrupal = date => moment(`${date}Z`, moment.ISO_8601).toDate();
+export const dateFromDrupal = date =>
+  moment(`${date}Z`, moment.ISO_8601).toDate();
 
-export const roundTo = (date, value = 15, units = 'minutes', method = 'ceil') => {
+export const roundTo = (
+  date,
+  value = 15,
+  units = "minutes",
+  method = "ceil"
+) => {
   const duration = moment.duration(value, units);
   return moment(Math[method](+date / +duration) * +duration);
 };
@@ -163,7 +171,7 @@ export const roundTo = (date, value = 15, units = 'minutes', method = 'ceil') =>
  * @return {Boolean}
  *   True if the user is super admin.
  */
-export const userIsSuperAdmin = () => getUserUid() === '1';
+export const userIsSuperAdmin = () => getUserUid() === "1";
 
 /**
  * Check if the current user has at least one of the provided roles.
@@ -192,11 +200,11 @@ export const userHasRole = roles =>
  */
 export const userIsStaff = () =>
   userHasRole([
-    'intercept_event_manager',
-    'intercept_event_organizer',
-    'intercept_staff',
-    'intercept_system_admin',
-    'intercept_room_reservation_approver',
+    "intercept_event_manager",
+    "intercept_event_organizer",
+    "intercept_staff",
+    "intercept_system_admin",
+    "intercept_room_reservation_approver"
   ]);
 
 /**
@@ -212,8 +220,8 @@ export const userIsStaff = () =>
  */
 export const userIsManager = () =>
   userHasRole([
-    'intercept_event_manager',
-    'intercept_event_organizer',
-    'intercept_system_admin',
-    'intercept_room_reservation_approver',
+    "intercept_event_manager",
+    "intercept_event_organizer",
+    "intercept_system_admin",
+    "intercept_room_reservation_approver"
   ]);
